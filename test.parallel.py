94a2f6df67c256416df90a4562477777aec32285
rolylolypoly <dangwilliam2@gmail.com>
2017-05-03 10:09:44 -0700

    Some documentation and cleanup.

--- test.parallel.py
+++ test.parallel.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from __future__ import print_function
 import RPi.GPIO as GPIO
 import threading
@@ -8,31 +9,16 @@ from dxl.dxlchain import DxlChain
 
 chain = DxlChain("/dev/ttyUSB0", rate=1000000)
 print(chain.get_motor_list())
-chain.goto(1, 0, 1023, True)
-chain.goto(1, 500, 1023, True)
-chain.goto(1, 0, 1023, True)
-chain.goto(2, 0, 1023, True)
-chain.goto(2, 500, 1023, True)
-chain.goto(2, 0, 1023, True)
-chain.goto(3, 0, 1023, True)
-chain.goto(3, 500, 1023, True)
-chain.goto(3, 0, 1023, True)
-chain.goto(4, 0, 1023, True)
-chain.goto(4, 500, 1023, True)
-chain.goto(4, 0, 1023, True)
-chain.goto(5, 0, 1023, True)
-chain.goto(5, 500, 1023, True)
-chain.goto(5, 0, 1023, True)
 
 
 class Encoder(object):
     """
-    4 Wire Encoder
+    A quadrature encoder
     """
     Rotary_counter = 0
     Current_A = 1
     Current_B = 1
-    motor = 0
+    id = 0
 
     LockRotary = threading.Lock()
 
@@ -43,8 +29,8 @@ class Encoder(object):
 
     def update_position(self, new_position):
         """
-        Updates position2
-        :param new_position: 
+        Makes sure that the position is sane
+        :param new_position: Position value
         """
         self.position += new_position * abs(new_position)
         if self.position > 1000:
@@ -57,11 +43,10 @@ class Encoder(object):
     def rotary_interrupt(self, a_or_b):
 
         """
-        Gets called when an edge is detected
-        :param a_or_b: Which pin initiated the call
-        :param enc_a: Pin 1
-        :param enc_b: Pin 2
+        Gets called when a change in voltage is detected
+        :param a_or_b: Which pin called back
         """
+
         switch_a = GPIO.input(self.enc_a)
         switch_b = GPIO.input(self.enc_b)
 
@@ -80,10 +65,10 @@ class Encoder(object):
             self.LockRotary.release()
         return
 
-    def __init__(self, enc_a, enc_b, motor):
+    def __init__(self, enc_a, enc_b, id):
         self.enc_a = enc_a
         self.enc_b = enc_b
-        self.motor = motor
+        self.id = id
 
         GPIO.setwarnings(True)
         GPIO.setmode(GPIO.BOARD)
@@ -94,11 +79,19 @@ class Encoder(object):
         GPIO.add_event_detect(self.enc_a, GPIO.RISING, callback=self.rotary_interrupt)
         GPIO.add_event_detect(self.enc_b, GPIO.RISING, callback=self.rotary_interrupt)
 
+    def startup(self):
+        """
+        Wiggles the finger 
+        """
+        chain.goto(self.id, 0, 1023, True)
+        chain.goto(self.id, 250, 1023, True)
+        chain.goto(self.id, 0, 1023, True)
+
 
 def update(encoder):
     """
-    Update position.
-    :param encoder: 
+    Update an Encoder's position value.
+    :param encoder: An Encoder object
     """
     encoder.LockRotary.acquire()
     new_position = encoder.Rotary_counter
@@ -114,14 +107,15 @@ Encoder2 = Encoder(11, 12, 2)
 Encoder3 = Encoder(15, 16, 3)
 Encoder4 = Encoder(21, 22, 4)
 Encoder5 = Encoder(23, 24, 5)
+array = [Encoder1, Encoder2, Encoder3, Encoder4, Encoder5]
+
+for encoder in array:
+    encoder.startup()
 
 while True:
     try:
-        update(Encoder1)
-        update(Encoder2)
-        update(Encoder3)
-        update(Encoder4)
-        update(Encoder5)
+        for encoder in array:
+            update(encoder)
 
     except KeyboardInterrupt:
         GPIO.cleanup()
