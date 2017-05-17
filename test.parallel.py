39edbf84cda1febfc3f2a3fbededae837fcab28a
rolylolypoly <dangwilliam2@gmail.com>
2017-05-17 09:50:05 -0700

    Added initial position and direction parameters. Some light refactoring.

--- test.parallel.py
+++ test.parallel.py
@@ -11,111 +11,117 @@ chain = DxlChain("/dev/ttyUSB0", rate=1000000)
 print(chain.get_motor_list())
 
 
-class Encoder(object):
+class Pair(object):
     """
-    A quadrature encoder
+    A quadrature encoder paired with one Dynamixel
     """
-    Rotary_counter = 0
-    Current_A = 1
-    Current_B = 1
-    id = 0
+    counter = 0
+    currentA = 1
+    currentB = 1
+    motor = 0
 
-    LockRotary = threading.Lock()
+    lockRotary = threading.Lock()
 
-    enc_a = 0
-    enc_b = 0
+    encoderA = 0
+    encoderB = 0
 
     position = 0
-
-    def update_position(self, new_position):
-        """
-        Makes sure that the position is sane
-        :param new_position: Position value
-        """
-        self.position += new_position * abs(new_position)
-        if self.position > 1000:
-            self.position = 1000
-        elif self.position < 0:
-            self.position = 0
-        else:
-            return
+    initialPosition = 0
+    direction = 0
 
     def rotary_interrupt(self, a_or_b):
 
         """
+        Interrupt callback
         Gets called when a change in voltage is detected
         :param a_or_b: Which pin called back
         """
 
-        switch_a = GPIO.input(self.enc_a)
-        switch_b = GPIO.input(self.enc_b)
+        switch_a = GPIO.input(self.encoderA)
+        switch_b = GPIO.input(self.encoderB)
 
-        if self.Current_A == switch_a and self.Current_B == switch_b:
+        if self.currentA == switch_a and self.currentB == switch_b:
             return
 
-        self.Current_A = switch_a
-        self.Current_B = switch_b
+        self.currentA = switch_a
+        self.currentB = switch_b
 
         if switch_a and switch_b:
-            self.LockRotary.acquire()
-            if a_or_b == self.enc_b:
-                self.Rotary_counter += 1
+            self.lockRotary.acquire()
+            if a_or_b == self.encoderB:
+                self.counter += 1
             else:
-                self.Rotary_counter -= 1
-            self.LockRotary.release()
+                self.counter -= 1
+            self.lockRotary.release()
         return
 
-    def __init__(self, enc_a, enc_b, id):
-        self.enc_a = enc_a
-        self.enc_b = enc_b
-        self.id = id
+    def __init__(self, encoderA, encoderB, motor, initialPosition, direction):
+        """
+        Initializes an encoder and a Dynamixel
+        :param encoderA: Encoder pin A 
+        :param encoderB: Encoder pin B
+        :param motor: Dynamixel ID number
+        :param initialPosition: Starting position of Dynamixel
+        :param direction: Direction that Dynamixel should turn
+        """
+        self.encoderA = encoderA
+        self.encoderB = encoderB
+        self.motor = motor
+        self.initialPosition = initialPosition
+        self.direction = direction
 
         GPIO.setwarnings(True)
         GPIO.setmode(GPIO.BOARD)
 
-        GPIO.setup(self.enc_a, GPIO.IN)
-        GPIO.setup(self.enc_b, GPIO.IN)
+        GPIO.setup(self.encoderA, GPIO.IN)
+        GPIO.setup(self.encoderB, GPIO.IN)
 
-        GPIO.add_event_detect(self.enc_a, GPIO.RISING, callback=self.rotary_interrupt)
-        GPIO.add_event_detect(self.enc_b, GPIO.RISING, callback=self.rotary_interrupt)
+        GPIO.add_event_detect(self.encoderA, GPIO.RISING, callback=self.rotary_interrupt)
+        GPIO.add_event_detect(self.encoderB, GPIO.RISING, callback=self.rotary_interrupt)
 
     def startup(self):
         """
         Wiggles the finger 
         """
-        chain.goto(self.id, 0, 1023, True)
-        chain.goto(self.id, 250, 1023, True)
-        chain.goto(self.id, 0, 1023, True)
+        chain.goto(self.motor, self.initialPosition, 1023, True)
+        chain.goto(self.motor, self.initialPosition + (250 * self.direction), 1023, True)
+        chain.goto(self.motor, self.initialPosition, 1023, True)
 
+    def update(self):
+        """
+        Update an Encoder's position value.
+        """
+        self.lockRotary.acquire()
+        new_position = self.counter
+        self.counter = 0
+        self.lockRotary.release()
 
-def update(encoder):
-    """
-    Update an Encoder's position value.
-    :param encoder: An Encoder object
-    """
-    encoder.LockRotary.acquire()
-    new_position = encoder.Rotary_counter
-    encoder.Rotary_counter = 0
-    encoder.LockRotary.release()
-    if new_position != 0:
-        encoder.update_position(new_position)
-        chain.goto(encoder.motor, encoder.position, 1000, False)
+        if new_position != 0:
+            self.position += new_position * abs(new_position) * 10
+            if self.position > 1000:
+                self.position = 1000
+            elif self.position < 0:
+                self.position = 0
+            else:
+                return
 
+            chain.goto(self.motor, self.position, 1000, False)
 
-Encoder1 = Encoder(8, 10, 1)
-Encoder2 = Encoder(11, 12, 2)
-Encoder3 = Encoder(15, 16, 3)
-Encoder4 = Encoder(21, 22, 4)
-Encoder5 = Encoder(23, 24, 5)
-array = [Encoder1, Encoder2, Encoder3, Encoder4, Encoder5]
+Pair1 = Pair(8, 10, 1, 0, 1)
+Pair2 = Pair(11, 12, 2, 0, 1)
+Pair3 = Pair(15, 16, 3, 0, 1)
+Pair4 = Pair(21, 22, 4, 0, 1)
+Pair5 = Pair(23, 24, 5, 0, 1)
+Pair6 = Pair(27, 28, 6, 0, 1)
+array = [Pair1, Pair2, Pair3, Pair4, Pair5, Pair6]
 
-for encoder in array:
-    encoder.startup()
+for Pair in array:
+    Pair.startup()
 
 while True:
     try:
-        for encoder in array:
-            update(encoder)
+        for Pair in array:
+            Pair.update()
 
     except KeyboardInterrupt:
         GPIO.cleanup()
